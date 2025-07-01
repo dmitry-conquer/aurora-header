9ef82a5e82a12c16acef7af257250e5addf40b5a
Dmitry Conquer <dmitry.conquer@gmail.com>
2025-07-01 09:24:08 +0300

    first commit

---
+++ src/scripts/main.ts
@@ -0,0 +1,6 @@
+import "../styles/main.scss";
+import { Header } from "./components";
+
+document.addEventListener("DOMContentLoaded", (): void => {
+  new Header();
+});
---
+++ vite.config.js
@@ -0,0 +1,72 @@
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  css: {
+    devSourcemap: true,
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+        silenceDeprecations: ["mixed-decls"],
+      },
+    },
+  },
+
+  build: {
+    appType: "mpa",
+    publicDir: "public",
+    modulePreload: false,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        pages: path.resolve(__dirname, "pages/index.html"),
+      },
+      output: {
+        assetFileNames: ({ name }) => {
+          if (/\.(css|scss|sass|less|styl|stylus)$/.test(name ?? "")) {
+            return "assets/style[extname]";
+          }
+          return "assets/[name][extname]";
+        },
+      },
+    },
+  },
+
+  plugins: [
+    handlebars({
+      partialDirectory: "components",
+    }),
+    {
+      name: "handlebars-watcher",
+      configureServer(server) {
+        const watcher = server.watcher;
+
+        watcher.add([path.resolve(__dirname, "components/**/*.html")]);
+
+        watcher.on("change", file => {
+          if (file.endsWith(".html")) {
+            server.ws.send({
+              type: "full-reload",
+              path: "*",
+            });
+          }
+        });
+      },
+    },
+    {
+      name: "wrap-in-iife",
+      generateBundle(outputOptions, bundle) {
+        Object.keys(bundle).forEach(fileName => {
+          const file = bundle[fileName];
+          if (fileName.endsWith(".js") && "code" in file) {
+            file.code = `(() => {\n${file.code}})()`;
+          }
+        });
+      },
+    },
+  ],
+});
